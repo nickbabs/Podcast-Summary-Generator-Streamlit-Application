ef3585ed3ba8ee9d7c0ae4add9bd8edc1a3d53dd
nickbabs <91480014+nickbabs@users.noreply.github.com>
2023-08-18 14:15:56 -0400

    podcast_frontend.py

--- podcast_frontend.py
+++ podcast_frontend.py
@@ -89,19 +89,19 @@ def main():
             st.subheader("Who are they?")
             st.write(podcast_info["podcast_guest"]['summary'])
 
-        def create_dict_from_json_files(folder_path):
-            json_files = [f for f in os.listdir(folder_path) if f.endswith('.json')]
-            data_dict = {}
-        
-            for file_name in json_files:
-                file_path = os.path.join(folder_path, file_name)
-                with open(file_path, 'r') as file:
-                    podcast_info = json.load(file)
-                    podcast_name = podcast_info['podcast_details']['podcast_title']
-                    # Process the file data as needed
-                    data_dict[podcast_name] = podcast_info
-        
-            return data_dict
+    def create_dict_from_json_files(folder_path):
+        json_files = [f for f in os.listdir(folder_path) if f.endswith('.json')]
+        data_dict = {}
+    
+        for file_name in json_files:
+            file_path = os.path.join(folder_path, file_name)
+            with open(file_path, 'r') as file:
+                podcast_info = json.load(file)
+                podcast_name = podcast_info['podcast_details']['podcast_title']
+                # Process the file data as needed
+                data_dict[podcast_name] = podcast_info
+    
+        return data_dict
         
     def process_podcast_info(url):
         f = modal.Function.lookup("corise-podcast-project", "process_podcast")
