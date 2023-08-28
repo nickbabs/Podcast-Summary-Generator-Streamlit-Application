9992357dc87986551f5929d3899a2507446e3fc2
nickbabs <91480014+nickbabs@users.noreply.github.com>
2023-08-28 00:27:03 -0400

    podcast_frontend.py

--- podcast_frontend.py
+++ podcast_frontend.py
@@ -11,7 +11,7 @@ def main():
     st.write("""Generate a summary about your favorite podcast's most recent episode on my page!
                 Simply copy and paste the link to your podcast's RSS feed on the left side of your screen
                 and click process. To find your podcast's RSS feed, search for your podcast's title on a
-                podcast search engine website such as listennotes.com and find the section that says RSS
+                podcast search engine website such as [listennotes.com](https://www.listennotes.com/) and find the section that says RSS
                 feed. This should provide you with a URL to the podcast's RSS feed.""")
 
     available_podcast_info = create_dict_from_json_files('.')
