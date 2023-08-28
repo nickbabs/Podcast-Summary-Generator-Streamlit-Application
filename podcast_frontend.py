73e6586c41d9c33d9f81175080110bb277c1938d
nickbabs <91480014+nickbabs@users.noreply.github.com>
2023-08-28 15:12:21 -0400

    podcast_frontend.py

--- podcast_frontend.py
+++ podcast_frontend.py
@@ -8,12 +8,12 @@ def main():
     
     # Right section - Newsletter content
     st.header("Let's get started...")
+    st.write("August 28, 2023:  **At this time, please choose a podcast with shorter episodes, must be under 30 minutes.**")
     st.write("""Generate a summary about your favorite podcast's most recent episode on my page! At the
                 left side of the screen, simply copy and paste the link to your podcast's RSS feed and click process. 
                 To find your podcast's RSS feed, search for your podcast's title on a podcast search engine
                 website such as [listennotes.com](https://www.listennotes.com/) and find the section that says RSS
-                feed. This should provide you with a URL to the podcast's RSS feed.
-                **At this time, please choose a podcast with shorter episodes, must be under 30 minutes.**""")
+                feed. This should provide you with a URL to the podcast's RSS feed.""")
 
     available_podcast_info = create_dict_from_json_files('.')
 
